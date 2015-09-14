eb71ac7d096c1f073bd4a9459ec801225768e6d6
Matt Coker <matt.coker@gmail.com>
2015-09-14 13:27:20 -0500

    include sendResponse post function on meetingController

--- assets/features/meeting/meetingController.js
+++ assets/features/meeting/meetingController.js
@@ -1,4 +1,9 @@
 angular.module('SocketToMe.meeting').controller('MeetingController', ['$scope', function ($scope) {
 
+  $scope.response = {};
+
+  $scope.sendResponse = function() {
+    io.socket.post('/response', $scope.response, function () {});
+  };
 
 }]);
