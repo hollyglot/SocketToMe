44ea5fc82a9ae24e6e8944b71436a6ea7abc82bd
Josh Gillespie <josh@bethel.io>
2015-09-14 12:35:46 -0500

    Finalizes logic for directing perspective users

--- assets/features/join/joinController.js
+++ assets/features/join/joinController.js
@@ -5,14 +5,15 @@ angular.module('SocketToMe.join').controller('JoinController', ['$scope', functi
 
   $scope.createJoinMeeting = function() {
 
-    io.socket.get('/meeting', $scope.meeting, function (existingMeeting) {
+    io.socket.get('/meeting', {name: $scope.meeting.name}, function (existingMeeting) {
+      console.log(existingMeeting.length, existingMeeting);
       if (existingMeeting.length) {
         // join the meeting if no password
         if (!$scope.meeting.password || $scope.meeting.password.length === 0) {
           console.log('Joining Meeting As participant');
           $ctrl.joinMeeting();
         } else {
-          if (existingMeeting.password === $scope.meeting.password) {
+          if (existingMeeting[0].password === $scope.meeting.password) {
             console.log('Joining Meeting As moderator');
             $scope.$root.modToken = $scope.meeting.name + $scope.meeting.password;
             // Redirect to moderate/meetingName
