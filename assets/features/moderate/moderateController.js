8a24c4c21378ba53d775e58bb28311ca7f757c4c
Matt Coker <matt.coker@gmail.com>
2015-09-22 23:45:44 -0500

    Use sockets to get question responses on switching to “review”

--- assets/features/moderate/moderateController.js
+++ assets/features/moderate/moderateController.js
@@ -1,5 +1,6 @@
-angular.module('SocketToMe.moderate').controller('ModerateController', ['$scope', '$stateParams', '$location', function ($scope, $stateParams, $location) {
+angular.module('SocketToMe.moderate').controller('ModerateController', ['$scope', '$stateParams', '$location', 'sailsSocket', function ($scope, $stateParams, $location, sailsSocket) {
 
+  $ctrl = this;
   $scope.questions = [];
   $scope.newQuestion = {};
   $scope.unansweredQuestions = [];
@@ -62,6 +63,33 @@ angular.module('SocketToMe.moderate').controller('ModerateController', ['$scope'
       io.socket.put('/question/' + $scope.$root.meeting.currentQuestion.id, {isDone: true}, function (updatedQuestion) {
         $scope.$root.meeting.currentQuestion = updatedQuestion;
         $scope.$apply();
+      });
+
+      // Get all reponses
+      $scope.allResponses = {stop: [],  start: [], cont: []};
+      $scope.createdResponses = {stop: [], start: [], cont: []};
+
+      sailsSocket.get('/meeting?name=' + $stateParams.meetingName).then(function (meeting) {
+
+        $scope.meeting = meeting[0];
+
+        io.socket.get('/response', {question: $scope.meeting.currentQuestion.id}, function (allResponses) {
+          console.log('all ', allResponses);
+
+          allResponses.forEach(function (response) {
+            if (response.type === 'stop') {
+              $scope.allResponses.stop.push(response.description);
+            } else if (response.type === 'start') {
+              $scope.allResponses.start.push(response.description);
+            } else {
+              $scope.allResponses.cont.push(response.description);
+            }
+          });
+
+          $scope.$apply();
+
+        });
+
       });
 
     } else {
