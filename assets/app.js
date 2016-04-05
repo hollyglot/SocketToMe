789831d82e8c76de983de8454ce0bc7f5299390f
Holly Gibson <hollyglotapps@gmail.com>
2016-04-05 09:58:16 -0500

    Add utility functions: zip and prepend

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,9 @@ angular.module('SocketToMe', [
   'SocketToMe.join',
   'SocketToMe.moderate',
   'SocketToMe.participate',
-  'bethel.sailsSocket'
+  'bethel.sailsSocket',
+  'ngSanitize',
+  'ngCsv'
 ])
 
 .config(['$urlRouterProvider', function ($urlRouterProvider) {
@@ -17,5 +19,44 @@ angular.module('SocketToMe', [
 
 }])
 
-.controller('AppCtrl', function() {});
+.controller('AppCtrl', function() {})
+
+.service('Utilities', function() {
+
+  /**
+   * zip() This function returns an array of tuples, where the i-th tuple
+   * contains the i-th element from each of the argument sequences or iterables.
+   * The returned array is extended in length to the length of the longest
+   * argument sequence with empty indices being represented as undefined.
+   *
+   * @param {Array} args
+   * @return {Array} array
+   */
+
+  this.zip = function () {
+    var args = [].slice.call(arguments);
+    var longest = args.reduce(function(a,b){
+      return a.length>b.length ? a : b
+    }, []);
+
+    return longest.map(function(_,i){
+      return args.map(function(array){return array[i]})
+    });
+  }
+
+  /**
+   * prepend() Add an item to the beginning of an array.
+   *
+   * @param {*} value
+   * @param {Array} array
+   * @return {Array} array
+   */
+  this.prepend = function (value, array) {
+    var newArray = array.slice(0);
+    newArray.unshift(value);
+    return newArray;
+  }
+})
+
+
 
